2d13d5f12bbccdf6ea937fd8ee51ac9388b35bfc
Vladyslav Pekker <agilesteel@gmail.com>
2020-04-08 10:30:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Installs [GitHub CLI 0.6.4](https://cli.github.com/) on [Ubuntu](https://www.ubuntu.com/)
+
+```bash
+wget -q https://script.install.devinsideyou.com/github-cli
+sudo chmod +x github-cli && ./github-cli
+```
+
+# Specifies [version](https://github.com/lihaoyi/Ammonite/releases) (optional)
+
+```bash
+wget -q https://script.install.devinsideyou.com/github-cli
+sudo chmod +x github-cli && ./github-cli 0.6.4
+```
---
+++ install-github-cli.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# remove yourself
+rm $0
+
+FALLBACK_VERSION="0.6.4"
+
+if [ "$1" != "" ]; then
+    VERSION="$1"
+else
+    VERSION=$FALLBACK_VERSION
+fi
+
+TARGET="gh_${VERSION}_linux_amd64.deb"
+URL="https://github.com/cli/cli/releases/download/v${VERSION}/${TARGET}"
+
+# install curl
+sudo apt install -yqqq curl
+
+# download GitHub CLI
+sudo curl -L $URL -o $TARGET
+
+# install GitHub CLI
+sudo dpkg -i $TARGET
+
+# remove dpkg
+rm -f $TARGET
+
+echo
+echo '"gh" is now on the path'
