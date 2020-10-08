34c1f58515ef1b14871406f74aad570d1f4e84f3
Vladyslav Pekker <agilesteel@gmail.com>
2020-10-08 19:39:38 -0300

    housekeeping

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
-# Installs [GitHub CLI 1.1.0](https://cli.github.com/) on [Ubuntu](https://www.ubuntu.com/)
+# Installs [GitHub CLI](https://cli.github.com/) on [Ubuntu](https://www.ubuntu.com/)
 
 ```bash
 curl -fLOs https://script.install.devinsideyou.com/github-cli
 sudo chmod +x github-cli && ./github-cli
-```
-
-# Specifies [version](https://github.com/lihaoyi/Ammonite/releases) (optional)
-
-```bash
-curl -fLOs https://script.install.devinsideyou.com/github-cli
-sudo chmod +x github-cli && ./github-cli 1.1.0
 ```
--- install-github-cli.sh
+++ install-github-cli.sh
@@ -3,25 +3,10 @@
 # remove yourself
 rm $0
 
-FALLBACK_VERSION="1.1.0"
-
-if [ "$1" != "" ]; then
-    VERSION="$1"
-else
-    VERSION=$FALLBACK_VERSION
-fi
-
-TARGET="gh_${VERSION}_linux_amd64.deb"
-URL="https://github.com/cli/cli/releases/download/v${VERSION}/${TARGET}"
-
-# download GitHub CLI
-curl -L $URL -o $TARGET
-
-# install GitHub CLI
-sudo dpkg -i $TARGET
-
-# remove dpkg
-rm -f $TARGET
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
 
 echo
 gh --version
